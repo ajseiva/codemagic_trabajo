9bf140bfe9a3cfd9c6e59504b5c63b8f51b14340
ajseiva <alvaro.jaenseiva@alum.uca.es>
2025-01-06 20:10:21 +0100

    Actualizar mensaje en App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,10 +4,7 @@ package com.example;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public static void main(String[] args) {
+    System.out.println("Hola desde Docker con Codemagic!"); // Cambiado
 }
+

9eee9f56b395e5f60be5af2b41d7f51e99f42626
ajseiva <alvaro.jaenseiva@alum.uca.es>
2025-01-06 20:57:13 +0100

    Actualizar App.java con nuevos cambios

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, esto es una prueba");
+        System.out.println("Hello, esto es una prueba buena");
     }
 }
 

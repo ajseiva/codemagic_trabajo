b4db7b6597968695bc70a89341ef9ece478a41c5
ajseiva <alvaro.jaenseiva@alum.uca.es>
2025-01-06 20:27:31 +0100

    Corregir errores en App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,10 +1,8 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public static void main(String[] args) {
-    System.out.println("Hola desde Docker con Codemagic!"); // Cambiado
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Codemagic!");
+    }
 }
 

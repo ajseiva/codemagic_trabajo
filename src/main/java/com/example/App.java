77dc469fa8ec6f8d88619f5b85320e8115d5673b
ajseiva <alvaro.jaenseiva@alum.uca.es>
2025-01-06 20:32:07 +0100

    Prueba: Cambiar mensaje en App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Codemagic!");
+        System.out.println("Hello, esto es una prueba");
     }
 }
 
